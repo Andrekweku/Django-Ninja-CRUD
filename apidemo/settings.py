10c17775dd029c5410e6d93191888dca58b6ba2c
andrekweku <32077921+Andrekweku@users.noreply.github.com>
2023-01-16 14:40:41 +0000

    Update settings.py

--- apidemo/settings.py
+++ apidemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-b7_i1ik4jvu^p(k5vr^*lt+v-&bqtv*nopt)#0!dwpr74yol=e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
